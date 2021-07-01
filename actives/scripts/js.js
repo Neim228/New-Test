396e1f6973469c46817c46dc750c234d138c2098
Neim <niklol1290@yandex.ru>
2021-07-01 18:31:19 +0300

    comit

---
+++ actives/scripts/js.js
@@ -0,0 +1,10 @@
+let a = 'text';
+function fun(b) {
+    if(b == '') {   
+        console.log("Нет элемента");
+    } else {
+        console.log("Есть елемент");
+    }
+    let el = document.querySelector('');
+}
+fun(a);
